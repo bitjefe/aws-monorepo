fbbe4c72c6f85e6c0165ba404bc48c9e9ba15a5e
jwiand <wiand.jeffrey@gmail.com>
2021-03-06 22:42:56 -0600

    header addition

--- host-web-app/src/App.js
+++ host-web-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Upon Host Builder
+          Welcome to the Upon Host Builder
         </p>
       </header>
       <AmplifySignOut />
