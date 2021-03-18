b3540fc6eaf9e744177d48538351f94c35d7be2b
jwiand <wiand.jeffrey@gmail.com>
2021-03-18 05:15:52 -0500

    Adds react-bootstrap, bootstrap and creates simple form with host data to collect

--- host-web-app/src/App.js
+++ host-web-app/src/App.js
@@ -1,19 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the Upon Host Builder!
-        </p>
-      </header>
-      <AmplifySignOut />
-    </div>
-  );
+import { Container, Button, Form } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+async function submitContactInfo() {
+    const data = {
+        body: {
+            hostType:formState.hostType,
+            businessName:formState.businessName,
+            locations:formState.locations,
+            state:formState.state,
+            email:formState.email,
+            message:formState.message
+        }
+    };
+    
+    console.log(data)
+    const apiData = await API.post('contact', '/new-host', data);
+    console.log({ apiData })
+    alert("Mail Sent");
 }
 
-export default withAuthenticator(App);
+const formState = { 
+    hostType:"",
+    businessName:"",
+    locations:"",
+    state:"",
+    email:"",
+    message:""
+}
+
+function updateFormState(key, value) {
+    formState[key] = value;
+}
+
+
+function App() {
+    return (
+        <Container>
+            <h3>Get in touch</h3>
+            <br />
+            <Form>
+                <Form.Group>
+                    <Form.Label>HostType</Form.Label>
+                    <Form.Control placeholder="Host Type" onChange={e => updateFormState('hostType', e.target.value)} />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Business Name</Form.Label>
+                    <Form.Control placeholder="Business Name" onChange={e => updateFormState('businessName', e.target.value)} />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Locations</Form.Label>
+                    <Form.Control placeholder="Locations" onChange={e => updateFormState('locations', e.target.value)} />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>State</Form.Label>
+                    <Form.Control placeholder="State" onChange={e => updateFormState('state', e.target.value)} />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control placeholder="Email" onChange={e => updateFormState('email', e.target.value)} />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control placeholder="Message" onChange={e => updateFormState('message', e.target.value)} />
+                </Form.Group>
+                <Button onClick={submitContactInfo}>Send a message</Button>
+            </Form>
+        </Container>
+    );
+}
+
+export default App;
