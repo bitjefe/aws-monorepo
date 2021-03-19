2f4c16af8fc1b1688947f6e7dcfd4b913ac07e4f
jwiand <wiand.jeffrey@gmail.com>
2021-03-19 17:42:16 -0500

    adds sass, custom radio buttons, card hover/active states

--- host-web-app/src/components/HostSelection/index.js
+++ host-web-app/src/components/HostSelection/index.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, Form } from 'react-bootstrap';
+
+import { AiOutlineGlobal } from "react-icons/ai"
+import { FiShoppingBag } from "react-icons/fi"
+import { BiBuildingHouse } from "react-icons/bi"
+import { RiHotelBedLine } from "react-icons/ri"
+import { IoAmericanFootballOutline } from "react-icons/io5"
 
 function HostSelectionPage() {
     let history = useHistory();
     const [hostType, setHostType] = useState("")
 
-    function handleClick() {
+    function handleContinue() {
         history.push("/home");
     }
     
@@ -46,74 +52,148 @@ function HostSelectionPage() {
                     <Row className="justify-content-center">
                         <Col xs={10} md={4} lg={3} xl={2}>
                             <Card
-                                id="hostCard"
-                                className="mt-2 w-full"
-                                style={{height:200, cursor:"pointer", background:"lightGray"}}
+                                className={hostType === "Accommodation" ? "mt-3 w-full bg-light active" : "mt-3 w-full bg-light"}
+                                style={{height:200, cursor:"pointer"}}
                                 onClick={() => setHostType("Accommodation")}
                                 >
                                 <Card.Body>
+                                    <RiHotelBedLine size={48} className="m-3"/>
                                     <Card.Text>
                                         Accomodation
                                     </Card.Text>
                                 </Card.Body>
+                                <Card.Footer className="bg-transparent border-0">
+                                    <Form>
+                                        <Form.Row className="align-items-center">
+                                            <Col xs="12">
+                                                <Form.Check
+                                                  custom
+                                                  type="radio"
+                                                  className="text-center"
+                                                  checked={hostType === "Accommodation"} 
+                                                />
+                                            </Col>
+                                        </Form.Row>
+                                    </Form>
+                                </Card.Footer>
                             </Card>
                         </Col>
                         <Col xs={10} md={4} lg={3} xl={2}>
                             <Card
-                                className="mt-2 w-full"
-                                style={{height:200, cursor:"pointer", background:"lightGray"}}
+                                className={hostType === "Real Estate" ? "mt-3 w-full bg-light active" : "mt-3 w-full bg-light"}
+                                style={{height:200, cursor:"pointer"}}
                                 onClick={() => setHostType("Real Estate")}
                                 >
                                 <Card.Body>
+                                    <BiBuildingHouse size={48} className="m-3" />
                                     <Card.Text>
                                         Real Estate
                                     </Card.Text>
                                 </Card.Body>
+                                <Card.Footer className="bg-transparent border-0">
+                                    <Form>
+                                        <Form.Row className="align-items-center">
+                                            <Col xs="12">
+                                                <Form.Check
+                                                  custom
+                                                  type="radio"
+                                                  className="text-center"
+                                                  checked={hostType === "Real Estate"} 
+                                                />
+                                            </Col>
+                                        </Form.Row>
+                                    </Form>
+                                </Card.Footer>
                             </Card>
                         </Col>
                         <Col xs={10} md={4} lg={3} xl={2}>
                             <Card
-                                className="mt-2 w-full"
-                                style={{height:200, cursor:"pointer", background:"lightGray"}}
+                                className={hostType === "Retail" ? "mt-3 w-full bg-light active" : "mt-3 w-full bg-light"}
+                                style={{height:200, cursor:"pointer"}}
                                 onClick={() => setHostType("Retail")}
                                 >
                                 <Card.Body>
+                                    <FiShoppingBag size={48} className="m-3"/>
                                     <Card.Text>
                                         Retail
                                     </Card.Text>
                                 </Card.Body>
+                                <Card.Footer className="bg-transparent border-0">
+                                    <Form>
+                                        <Form.Row className="align-items-center">
+                                            <Col xs="12">
+                                                <Form.Check
+                                                  custom
+                                                  type="radio"
+                                                  className="text-center"
+                                                  checked={hostType === "Retail"} 
+                                                />
+                                            </Col>
+                                        </Form.Row>
+                                    </Form>
+                                </Card.Footer>
                             </Card>
                         </Col>
                         <Col xs={10} md={4} lg={3} xl={2}>
                             <Card
-                                className="mt-2 w-full"
-                                style={{height:200, cursor:"pointer", background:"lightGray"}}
+                                className={hostType === "Arts, Ent & Rec" ? "mt-3 w-full bg-light active" : "mt-3 w-full bg-light"}
+                                style={{height:200, cursor:"pointer"}}
                                 onClick={() => setHostType("Arts, Ent & Rec")}
                                 >
                                 <Card.Body>
+                                    <IoAmericanFootballOutline size={48} className="m-3"/>
                                     <Card.Text>
                                         Arts, Ent & Rec
                                     </Card.Text>
                                 </Card.Body>
+                                <Card.Footer className="bg-transparent border-0">
+                                    <Form>
+                                        <Form.Row className="align-items-center">
+                                            <Col xs="12">
+                                                <Form.Check
+                                                  custom
+                                                  type="radio"
+                                                  className="text-center"
+                                                  checked={hostType === "Arts, Ent & Rec"} 
+                                                />
+                                            </Col>
+                                        </Form.Row>
+                                    </Form>
+                                </Card.Footer>
                             </Card>
                         </Col>
                         <Col xs={10} md={4} lg={3} xl={2}>
                             <Card
-                                className="mt-2 w-full"
-                                style={{height:200, cursor:"pointer", background:"lightGray"}}
+                                className={hostType === "Transportation" ? "mt-3 w-full bg-light active" : "mt-3 w-full bg-light"}
+                                style={{height:200, cursor:"pointer"}}
                                 onClick={() => setHostType("Transportation")}
                                 >
                                 <Card.Body>
+                                    <AiOutlineGlobal size={48} className="m-3"/>
                                     <Card.Text>
                                         Transportation
                                     </Card.Text>
                                 </Card.Body>
+                                <Card.Footer className="bg-transparent border-0">
+                                    <Form>
+                                        <Form.Row className="align-items-center">
+                                            <Col xs="12">
+                                                <Form.Check
+                                                  custom
+                                                  type="radio"
+                                                  className="text-center"
+                                                  checked={hostType === "Transportation"} 
+                                                />
+                                            </Col>
+                                        </Form.Row>
+                                    </Form>
+                                </Card.Footer>
                             </Card>
                         </Col>
                     </Row>
                     <Row>
-                        <Col className="pt-5">
-                            <Button onClick={handleClick} disabled={hostType === ""}>
+                        <Col className="p-5">
+                            <Button onClick={handleContinue} disabled={hostType === ""}>
                                 Continue
                             </Button>
                         </Col>
--- host-web-app/src/index.js
+++ host-web-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import './custom.scss';
+
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
